ac8315e7b417f0bfbbb24d101af992e87599a262
CRIKELER <ndnathanduong@gmail.com>
2026-05-27 17:20:24 -0500

    Add custom formats for Potassium rulesets

--- config/custom-formats.ts
+++ config/custom-formats.ts
@@ -1,1 +1,21 @@
 
+export const Formats: import('../sim/dex-formats').FormatList = [
+	{
+		section: "Potassium",
+	},
+	{
+		name: "Potassium Extended OU",
+		mod: 'gen9',
+		ruleset: ['Standard', 'Evasion Abilities Clause'],
+		banlist: ['Uber', 'AG', 'Moody', 'King\'s Rock', 'Razor Fang'],
+	},
+	{
+		name: "Potassium Extended Doubles OU",
+		mod: 'gen9',
+		gameType: 'doubles',
+		ruleset: ['Standard Doubles'],
+		banlist: ['DUber'],
+	},
+
+	
+]
