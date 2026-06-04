7b0800a96b7cb04aa17a730d49345ea74e397699
awesomesauce <duongdylan2008@gmail.com>
2026-06-04 16:08:41 -0500

    ok

--- config/custom-formats.ts
+++ config/custom-formats.ts
@@ -6,15 +6,15 @@ export const Formats: import('../sim/dex-formats').FormatList = [
 	{
 		name: "Potassium Extended OU",
 		mod: 'gen9',
-		ruleset: ['Standard NatDex', 'Evasion Abilities Clause'],
-		banlist: ['Uber', 'AG', 'Moody', 'King\'s Rock', 'Razor Fang', 'Terastal Clause'],
+		ruleset: ['Standard NatDex', 'Evasion Abilities Clause', 'Terastal Clause'],
+		banlist: ['Uber', 'AG', 'Moody', 'King\'s Rock', 'Razor Fang'],
 	},
 	{
 		name: "Potassium Extended Doubles OU",
 		mod: 'gen9',
 		gameType: 'doubles',
-		ruleset: ['Standard Doubles', 'NatDex Mod','Evasion Abilities Clause'],
-		banlist: ['DUber', 'Terastal Clause'],
+		ruleset: ['Standard Doubles', 'NatDex Mod','Evasion Abilities Clause', 'Terastal Clause'],
+		banlist: ['DUber'],
 	},
 
 	
