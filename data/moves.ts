e5fbb4dabeab4eec1d2acdb31ed4ff0905837ae3
CRIKELER <ndnathanduong@gmail.com>
2026-07-01 18:44:40 -0500

    hc fix

--- data/moves.ts
+++ data/moves.ts
@@ -22023,7 +22023,7 @@ export const Moves: import('../sim/dex-moves').MoveDataTable = {
 		name: "Haunted Cry",
 		pp: 20,
 		priority: 0,
-		flags: { protect: 1, mirror: 1 },
+		flags: { protect: 1, mirror: 1, sound: 1, bypasssub: 1 },
 		secondary: {
 			chance: 100,
 			boosts: {
